f8a21a8774b6f632ed84bdfef2ceb17aa8de5595
mgr-inz-rafal <rchabowski@gmail.com>
2018-11-30 21:11:10 +0100

    Protection against crash when calculating 0-length path

--- src/path.cc
+++ src/path.cc
@@ -38,6 +38,11 @@ bool path::calculate(point<int16_t> start, point<int16_t> end) {
         return false;
     }
 
+    // Don't search for path withing one subsector
+    if (str.first == etr.first) {
+        return false;
+    }
+
     visited_subsectors.clear();
     visited_subsectors.insert(static_cast<int16_t>(str.first));
 
